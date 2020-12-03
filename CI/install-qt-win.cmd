94ee3e0dbfb7115b98f8fdf579613767a5a1cd88
ludo <ludovic.roux@cosmosoftware.io>
2020-12-03 17:29:08 +0800

    windows: command move

--- CI/install-qt-win.cmd
+++ CI/install-qt-win.cmd
@@ -1,4 +1,4 @@
 if exist Qt_5.10.1.7z (curl -kLO https://cdn-fastly.obsproject.com/downloads/Qt_5.10.1.7z -f --retry 5 -z Qt_5.10.1.7z) else (curl -kLO https://cdn-fastly.obsproject.com/downloads/Qt_5.10.1.7z -f --retry 5 -C -)
 7z x Qt_5.10.1.7z -oQt
-Move-Item Qt C:\QtDep
+move Qt C:\QtDep
 dir C:\QtDep
