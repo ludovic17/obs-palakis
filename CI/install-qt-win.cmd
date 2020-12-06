1b2fa7861c3fe145d98b14833a608861eda97dba
ludo <ludovic.roux@cosmosoftware.io>
2020-12-06 20:42:40 +0800

    windows: using command 'mv' instead of 'move'

--- CI/install-qt-win.cmd
+++ CI/install-qt-win.cmd
@@ -1,6 +1,7 @@
 if not exist C:\QtDep\ (
   if exist Qt_5.10.1.7z (curl -kLO https://cdn-fastly.obsproject.com/downloads/Qt_5.10.1.7z -f --retry 5 -z Qt_5.10.1.7z) else (curl -kLO https://cdn-fastly.obsproject.com/downloads/Qt_5.10.1.7z -f --retry 5 -C -)
   7z x Qt_5.10.1.7z -oQt
-  move Qt C:\QtDep
+  mv Qt C:\QtDep
+#  move Qt C:\QtDep
   dir C:\QtDep
 )
