58f73ed1fec8bac518b25bcb6829cc4dca65dd29
ludo <ludovic.roux@cosmosoftware.io>
2020-12-06 12:09:40 +0800

    windows: check if Qt directory exists

--- CI/install-qt-win.cmd
+++ CI/install-qt-win.cmd
@@ -1,4 +1,6 @@
-if exist Qt_5.10.1.7z (curl -kLO https://cdn-fastly.obsproject.com/downloads/Qt_5.10.1.7z -f --retry 5 -z Qt_5.10.1.7z) else (curl -kLO https://cdn-fastly.obsproject.com/downloads/Qt_5.10.1.7z -f --retry 5 -C -)
-7z x Qt_5.10.1.7z -oQt
-move Qt C:\QtDep
-dir C:\QtDep
+if not exist C:\QtDep\ (
+  if exist Qt_5.10.1.7z (curl -kLO https://cdn-fastly.obsproject.com/downloads/Qt_5.10.1.7z -f --retry 5 -z Qt_5.10.1.7z) else (curl -kLO https://cdn-fastly.obsproject.com/downloads/Qt_5.10.1.7z -f --retry 5 -C -)
+  7z x Qt_5.10.1.7z -oQt
+  move Qt C:\QtDep
+  dir C:\QtDep
+)
