dc283e810d9a153f44e94f27cf384f19d3f9790b
ludo <ludovic.roux@cosmosoftware.io>
2020-12-09 18:46:48 +0800

    windows: install wixtoolset and call cpack

--- CI/before-deploy-win.cmd
+++ CI/before-deploy-win.cmd
@@ -1,3 +1,9 @@
-robocopy .\build32\rundir\Release .\build\ /E /XF .gitignore
-robocopy .\build64\rundir\Release .\build\ /E /XC /XN /XO /XF .gitignore
-7z a build.zip .\build\*
+REM robocopy .\build32\rundir\Release .\build\ /E /XF .gitignore
+REM robocopy .\build64\rundir\Release .\build\ /E /XC /XN /XO /XF .gitignore
+REM 7z a build.zip .\build\*
+
+REM install WiX Toolset
+choco -y install wixtoolset
+
+cd build64
+cpack -G WIX
